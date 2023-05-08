9d40c8017851201d5c47cfe38d073366c96ef4ea
Oliver Lu <pa596bu500@gmail.com>
2023-05-08 01:34:02 +0000

    add postMeterKiv2

--- src/main/java/com/pabu5h/evs2/oqghelper/QueryHelper.java
+++ src/main/java/com/pabu5h/evs2/oqghelper/QueryHelper.java
@@ -445,6 +445,79 @@ public class QueryHelper {
             }
         }
     }
+    public void postMeterKiv2(String meterSnStr,
+                              String kivTag,
+                              String postDateTimeStr,
+                              long numOfEvents,
+                              String kivRef,
+                              double kivVal,
+                              String postedBy,
+                              String sessionId){
+        String meterKivTable = "meter_kiv";
+
+        if(postDateTimeStr == null || postDateTimeStr.isEmpty()) {
+            postDateTimeStr = DateTimeUtil.getZonedDateTimeStr(LocalDateTime.now(), ZoneId.of("Asia/Singapore"));;
+        }
+        if(sessionId == null || sessionId.isEmpty()) {
+            sessionId = UUID.randomUUID().toString();
+        }
+
+        if(!meterSnExistsInMeterTable(meterSnStr)){
+            logger.info("meter_sn: " + meterSnStr + " does not exist in meter table");
+            return;
+        }
+
+        //check if the record for that session_id exists
+        String sqlCheck = "select id, number_of_events from " + meterKivTable +
+                " where meter_sn = '" + meterSnStr +
+                "' and session_id = '" + sessionId + "'";
+        List<Map<String, Object>> meterKiv = new ArrayList<>();
+        try {
+            meterKiv = oqgHelper.OqgR(sqlCheck);
+        } catch (Exception e) {
+            logger.error("Error getting meterKiv for meterSn: " + meterSnStr);
+            throw new RuntimeException(e);
+        }
+        if(meterKiv.isEmpty()) {
+            Map<String, Object> kivSqlMap = Map.of("table", meterKivTable,
+                    "content", Map.of(
+                            "meter_sn", meterSnStr,
+                            "kiv_tag", kivTag,
+                            "kiv_start_timestamp", postDateTimeStr,
+                            "number_of_events", numOfEvents,
+                            "kiv_status","posted",
+                            "kiv_ref", kivRef,
+                            "kiv_val", kivVal,
+                            "posted_by", postedBy,
+                            "session_id", sessionId
+                    ));
+            Map<String, String> sqlInsert = SqlUtil.makeInsertSql(kivSqlMap);
+            if(sqlInsert.get("sql")==null){
+                logger.error("Error getting insert sql for meterKiv for meterSn: " + meterSnStr);
+                throw new RuntimeException("Error getting insert sql for meterKiv for meterSn: " + meterSnStr);
+            }
+            try {
+                oqgHelper.OqgIU(sqlInsert.get("sql"));
+            } catch (Exception e) {
+                logger.error("Error inserting meterKiv for meterSn: " + meterSnStr);
+                throw new RuntimeException(e);
+            }
+        } else {
+            //update the record
+            long id = MathUtil.ObjToLong(meterKiv.get(0).get("id"));
+            long numOfEventsOld = MathUtil.ObjToLong(meterKiv.get(0).get("number_of_events")==null?0:meterKiv.get(0).get("number_of_events"));
+            long numOfEventsNew = numOfEventsOld + numOfEvents;
+            String sqlUpdate = "update " + meterKivTable + " set number_of_events = " + numOfEventsNew +
+                    " where id = " + id;
+            try {
+                oqgHelper.OqgIU(sqlUpdate);
+            } catch (Exception e) {
+                logger.error("Error updating meterKiv for meterSn: " + meterSnStr);
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
 
     public void postOpLog(String postDateTimeStr,
                           String username,
