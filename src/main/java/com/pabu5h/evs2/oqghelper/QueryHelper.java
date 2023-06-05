622a3fbce7d4702c60377a2e00b4867ec12c0602
Oliver Lu <pa596bu500@gmail.com>
2023-06-05 05:22:46 +0000

    add concentrator_id to meter info list

--- src/main/java/com/pabu5h/evs2/oqghelper/QueryHelper.java
+++ src/main/java/com/pabu5h/evs2/oqghelper/QueryHelper.java
@@ -113,7 +113,7 @@ public class QueryHelper {
         return meterInfo;
     }
     public List<Map<String, Object>> getAllMeterInfo2(){
-        String sql = "select meter_sn, meter_displayname, reading_interval, commission_timestamp, " +
+        String sql = "select meter_sn, meter_displayname, reading_interval, concentrator_id, commission_timestamp, " +
                 " mms_address, mms_block, mms_building, " +
                 " esim_id, data_subscription_id " +
                 " from meter";
